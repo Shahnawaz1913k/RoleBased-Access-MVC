cd992932360ba1027c14e8cf030af54815cb6b12
Shahnawaz alam <sa467546@gmail.com>
2025-08-30 11:14:28 +0530

    UI modified

--- RoleBased/obj/Debug/net9.0/RoleBased.AssemblyInfo.cs
+++ RoleBased/obj/Debug/net9.0/RoleBased.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoleBased")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebca87c0033489cdf415f0c2f01c9901cdd5cf9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleBased")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleBased")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
